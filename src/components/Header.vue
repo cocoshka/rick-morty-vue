e14651499077c8fa72c28ef60d9597706a91a9c6
Marcin Kokoszka <mcocoshka@gmail.com>
2022-03-06 12:15:23 +0100

    Add basic header and navigation

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <img src="@/assets/img/logo.png" alt="Rick and Morty logo" />
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  display: grid;
+  align-content: center;
+  height: 134px;
+  padding-left: 140px;
+  padding-right: 140px;
+  border-bottom: 1px solid $light-gray;
+}
+</style>

e5bf7639d834c5f5818319ecdaee4663b37fa411
Marcin Kokoszka <mcocoshka@gmail.com>
2022-03-11 10:41:02 +0100

    Add generic Table component (#1)
    * Try to make generic table component
    
    * Finish making generic Table component

---
+++ src/models/Location.ts
@@ -0,0 +1,13 @@
+import type Character from "./Character";
+
+type Location = {
+  id: number;
+  name: string;
+  type: string;
+  dimension: string;
+  residents: Character[];
+  url: string;
+  created: string;
+};
+
+export default Location;
--- src/views/AllCharactersView.vue
+++ src/views/AllCharactersView.vue
@@ -1,5 +1,71 @@
-<script setup lang="ts"></script>
+<script setup lang="tsx">
+import type Character from "@/models/Character";
+import type { TableColumns } from "@/models/Table";
+import { useCharactersStore } from "@/stores/characters";
+import { GenericTable } from "../components/Table.vue";
+
+const charactersStore = useCharactersStore();
+charactersStore.fetchCharacters();
+
+const headers: TableColumns<Character> = [
+  {
+    title: "Photo",
+    render(row) {
+      return (
+        <img
+          src={row.image}
+          width="43"
+          height="68"
+          style="object-fit: cover;"
+        />
+      );
+    },
+  },
+  {
+    title: "Name",
+    field: "name",
+  },
+  {
+    title: "Gender",
+    field: "gender",
+  },
+  {
+    title: "Species",
+    field: "name",
+  },
+  {
+    title: "Name",
+    field: "species",
+  },{
+    title: "Last Episode",
+    render(row) {
+      return row.episode[row.episode.length - 1].episode;
+    },
+  },
+  {
+    title: "Add To Favourites",
+    render: (row) => {
+      return (
+        <>
+          <button>test</button>
+        </>
+      );
+    },
+  }
+];
+</script>
+<script lang="tsx">
+export default {
+  components: {
+    CharacterTable: GenericTable<Character>(),
+  },
+};
+</script>
 
 <template>
+  <CharacterTable
+    :headers="headers"
+    :data="charactersStore.getAllCharacters"
+  ></CharacterTable>
   <main>Characters</main>
 </template>

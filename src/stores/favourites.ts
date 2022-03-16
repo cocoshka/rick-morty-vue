07fe62afa432eec22f6f7ba9b6d0114b9ea88069
Marcin Kokoszka <mcocoshka@gmail.com>
2022-03-16 09:38:52 +0100

    Add favourites table and store

---
+++ src/stores/favourites.ts
@@ -0,0 +1,26 @@
+import type Character from "@/models/Character";
+import { defineStore } from "pinia";
+
+type State = {
+  favourites: Record<number, Character>;
+};
+
+export const useFavouritesStore = defineStore({
+  id: "favourites",
+  state: (): State => ({
+    favourites: {},
+  }),
+  getters: {
+    getFavourites(): Character[] {
+      return Object.values(this.favourites);
+    },
+  },
+  actions: {
+    addFavourite(character: Character): void {
+        this.favourites[character.id] = character;
+    },
+    removeFavourite(id: number): void {
+      delete this.favourites[id];
+    }
+  },
+});
--- src/views/FavouritesView.vue
+++ src/views/FavouritesView.vue
@@ -1,5 +1,70 @@
-<script setup lang="ts"></script>
+<script setup lang="tsx">
+import type Character from "@/models/Character";
+import type { TableColumns } from "@/models/Table";
+import { useFavouritesStore } from "@/stores/favourites";
+import { GenericTable } from "../components/Table.vue";
+
+const favouritesStore = useFavouritesStore();
+
+const headers: TableColumns<Character> = [
+  {
+    title: "Photo",
+    render(row) {
+      return (
+        <img
+          src={row.image}
+          width="43"
+          height="68"
+          style="object-fit: cover;"
+        />
+      );
+    },
+  },
+  {
+    title: "Name",
+    field: "name",
+  },
+  {
+    title: "Gender",
+    field: "gender",
+  },
+  {
+    title: "Species",
+    field: "name",
+  },
+  {
+    title: "Name",
+    field: "species",
+  },{
+    title: "Last Episode",
+    render(row) {
+      return row.episode[row.episode.length - 1].episode;
+    },
+  },
+  {
+    title: "Remove From Favourites",
+    render: (row) => {
+      return (
+        <>
+          <button onClick={() => favouritesStore.removeFavourite(row.id)}>Remove</button>
+        </>
+      );
+    },
+  }
+];
+</script>
+<script lang="tsx">
+export default {
+  components: {
+    CharacterTable: GenericTable<Character>(),
+  },
+};
+</script>
 
 <template>
-  <main>Favourites</main>
+  <CharacterTable
+    :headers="headers"
+    :data="favouritesStore.getFavourites"
+  ></CharacterTable>
+  <main>Characters</main>
 </template>

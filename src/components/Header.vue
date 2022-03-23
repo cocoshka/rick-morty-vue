4dcbce2cf9a31def115141777a8352e80d7419dd
Marcin Kokoszka <mcocoshka@gmail.com>
2022-03-23 15:16:26 +0100

    Add search bar component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,28 @@
+<script lang="ts" setup>
+import Spacer from "./Spacer.vue";
+import SearchBar from "./SearchBar.vue";
+
+const filters = {
+  name: "Name",
+  identifier: "Identifier",
+  episode: "Episode",
+};
+
+</script>
+
 <template>
   <header>
-    <img src="@/assets/img/logo.png" alt="Rick and Morty logo" />
+      <img src="@/assets/img/logo.png" alt="Rick and Morty logo" />
+      <Spacer width="80px" />
+      <SearchBar :filters="filters" />
   </header>
 </template>
 
 <style lang="scss" scoped>
 header {
-  display: grid;
-  align-content: center;
+  display: flex;
+  align-items: center;
+  // align-content: center;
   height: 134px;
   padding-left: 140px;
   padding-right: 140px;
